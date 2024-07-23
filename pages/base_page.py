83dd353c1d3b061afd902f811ccd1c8ce50b5bf4
den_kho <20597299+denkho@users.noreply.github.com>
2024-07-23 22:28:27 +0300

    tests the possibility to go to login page from product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ import math
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -13,7 +14,16 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-    
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+
+    def should_be_login_link(self):
+        assert self.browser.find_element(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
-import time
 import pytest
+
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.ulrs import Urls
 
@@ -64,6 +65,7 @@ def test_guest_can_add_product_to_basket_02(browser, link):
         f"The price of product added to basket was '{product_price_added_to_basket}', but it is '{price_of_product_in_basket}'"
 
 
+@pytest.mark.skip
 @pytest.mark.xfail(reason="should fail")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, Urls.PRODUCT_CODERS_AT_WORK)
@@ -72,15 +74,32 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, Urls.PRODUCT_CODERS_AT_WORK)
     page.open()
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 @pytest.mark.xfail(reason="the success message doesn't disappear")
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, Urls.PRODUCT_CODERS_AT_WORK)
     page.open()
     page.add_to_basket_click()
     assert not page.should_be_disapeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_page()
